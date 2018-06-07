4af9e28541707136728bbb3ab6ee6d03b67b94fa
dashimaki360 <17.punch@gmail.com>
2018-06-07 21:51:20 +0900

    tone and volm

--- monotron.py
+++ monotron.py
@@ -1,26 +1,31 @@
 # -*- coding: utf-8 -*-
-
-import wiringpi2 as w
+import pigpio
 import time
 
 
-
 def monotron() :
-     # 音程をコントロール
-     REF=3.3  # 5.p or 3.3
-     w.wiringPiSetup()
-     w.pinMode(0,1)
-     w.pinMode(1,2)
-     w.digitalWrite(0,1)
-     while 1 :
-         for i in [0,300,380,460,540,640,720,1000] :
-             w.pwmWrite(1,i)
-             print(i)
-             time.sleep(0.5)
-         w.digitalWrite(0,0)
-
+    a = 0
+    b = 300
+    c = 380
+    d = 460
+    e = 540
+    f = 640
+    g = 720
+    aa = 1000
+    # 音程をコントロール
+    REF=3.3  # 5.p or 3.3
+    pi = pigpio.pi()
+    pi.set_mode(17,pigpio.OUTPUT) # volume
+    pi.set_mode(18,pigpio.OUTPUT) # tone
+    pi.write(17,1)
+    while 1 :
+        #for i in [0,300,380,460,540,640,720,1000] :
+        for i in [b,c,d,e,f,g,aa]:
+            pi.hardware_PWM(18,16,i*1000)
+            print(i)
+            time.sleep(1)
 
 if __name__ == '__main__' :
-	monotron();
+    monotron();
 
 
